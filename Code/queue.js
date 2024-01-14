23c07deb36efb2b520df2ee93fb511ffb2da9cd4
your_username <70048174@THAIBEV.COM>
2024-01-14 14:15:22 +0700

    edit script

--- Code/queue.js
+++ Code/queue.js
@@ -5,7 +5,6 @@ function callQueueCustomer(queueNumber, counterService){
 
     console.log("เชิญลูกค้า หมายเลข"+ " "+ queueNumber +" " + "ที่ช่องบริการ หมายเลข " + counterService);
    
-
 }
 
 callQueueCustomer(230, 3);
@@ -14,7 +13,6 @@ callQueueCustomer(231, 1);
 //ถ้า ลูกค้า 1-4 ท่าน รับคิว A
 //มากกว่า5 ท่าน รับคิว B 
 function callReceiveQueue(amountCustomer, queue){
-
     console.log("ลูกค้า จำนวน " + amountCustomer +" ท่าน"+" " +  "กรุณา รับคิวบริการ " + queue);
 }
 
