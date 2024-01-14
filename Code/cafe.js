e82aedd92d20f9e05560f9045240b2f4910e3715
your_username <70048174@THAIBEV.COM>
2024-01-14 10:41:35 +0700

    edit

--- Code/cafe.js
+++ Code/cafe.js
@@ -1,8 +1,8 @@
-let cafeName  = "Enjoy Cafe"
-let customer = "Anny"
+let cafeName  = "Enjoy Cafe";
+let customer = "Anny";
 
-message = "Welcome To"+" " + cafeName;
-orderReadyMessage = "เครื่องดื่ม ของคุณ"+" " + customer + " "+ "ได้แล้วค่ะ"
+let message = "Welcome To"+" : " + cafeName;
+let orderReadyMessage = "เครื่องดื่ม ของคุณ"+" " + customer + " "+ "ได้แล้วค่ะ";
 
-console.log(message)
-console.log(orderReadyMessage)
+console.log(message);
+console.log(orderReadyMessage);
