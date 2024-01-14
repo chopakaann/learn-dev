81b06bdbf569de1d8a2a0d6d00487465e187696b
your_username <70048174@THAIBEV.COM>
2024-01-14 09:47:50 +0700

    test2

--- Code/index.js
+++ Code/index.js
@@ -1,2 +1,9 @@
-console.log("Hello boss")
-console.log("Chopaka Akkapatarakosit")
+// console.log("Hello boss")
+// console.log("Chopaka Akkapatarakosit")
+// console.log(32)
+
+let Name = "Chopaka Akkapatarakosit"
+let Age = 32
+console.log(Name, Age)
+
+
