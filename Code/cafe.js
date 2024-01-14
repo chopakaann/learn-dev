691ed9f3019e231ba699c8454044ea43f265cfdf
your_username <70048174@THAIBEV.COM>
2024-01-14 10:34:50 +0700

    add script cafe

---
+++ Code/cafe.js
@@ -0,0 +1,8 @@
+let cafeName  = "Enjoy Cafe"
+let customer = "Anny"
+
+message = "Welcome To"+" " + cafeName;
+orderReadyMessage = "เครื่องดื่ม ของคุณ"+" " + customer + " "+ "ได้แล้วค่ะ"
+
+console.log(message)
+console.log(orderReadyMessage)
--- Code/index.js
+++ Code/index.js
@@ -6,4 +6,8 @@ let Name = "Chopaka Akkapatarakosit"
 let Age = 32
 console.log(Name, Age)
 
-
+// let Name = "Chopaka Akkapatarakosit"
+// console.log(Name)
+// //เปลี่ยนชื่อ ตอน log จะออกมาทั้งสอง 
+// Name = "Chopaka Akkapatarakosit 2"
+// console.log(Name)
