a30269d80b2db190e220900cdeb6bca62e398496
your_username <70048174@THAIBEV.COM>
2024-02-05 20:31:47 +0700

    edit script test cafe

--- Code/cafe_1.js
+++ Code/cafe_1.js
@@ -9,4 +9,21 @@ function sayHelloCustomer (customerName) {
     
     console.log(sayHelloCustomer());
     
-    module.exports = sayHelloCustomer;
+    // module.exports = sayHelloCustomer;
+
+
+    function noticeOrderMessage (customerName, statusOrder, menu) {
+
+        if (statusOrder === "In Process" ){
+            return menu +" "+ "ของคุณ "+customerName +" "+ "กำลังทำอยู่นะคะ กรุณารอสักครู่ค่ะ";
+    
+        }else if (statusOrder === "Success"){
+            return menu +" "+ "ของคุณ "+ customerName+ " "+"เสร็จแล้ว เชิญรับได้ที่เค้าเตอร์ค่ะ";
+        }
+            return "กำลังทำ"+ menu + " ของคุณ " + customerName +" "+"อยู่นะคะ กรุณารอสักครู่ค่ะ"
+    }
+        
+        console.log(noticeOrderMessage());
+        
+        //กรณีที่มีการส่ง ไป 2 function
+        module.exports = {sayHelloCustomer, noticeOrderMessage};
--- Code/cafe_1.test.js
+++ Code/cafe_1.test.js
@@ -1,15 +1,44 @@
-const sayHello = require("./cafe_1.js");
+// const sayHelloCustomer = require("./cafe_1.js");
+
+const {sayHelloCustomer, noticeOrderMessage} = require("./cafe_1.js");
 
 test("VIP customer walk in cafe", () => {
 
-    const sayHelloCustomerMsg = sayHello("คุณแอน");
+    const sayHelloCustomerMsg = sayHelloCustomer("คุณแอน");
     expect(sayHelloCustomerMsg).toBe("สวัสดีค่ะ คุณแอน วันนี้รับอะไรดีคะ"); 
 
 });
 
 test("Other customer walk in cafe", () => {
 
-    const sayHelloCustomerMsg = sayHello("");
+    const sayHelloCustomerMsg = sayHelloCustomer("");
     expect(sayHelloCustomerMsg).toBe("สวัสดีค่ะ คุณลูกค้า รับอะไรดีคะ"); 
 
+});
+
+
+test("notice order in progress", () => {
+
+    const aleartMessage = noticeOrderMessage("Ann","In Process","กาแฟเย็น");
+    expect(aleartMessage).toBe("กาแฟเย็น ของคุณ Ann กำลังทำอยู่นะคะ กรุณารอสักครู่ค่ะ"); 
+  
+});
+
+test("notice order Success", () => {
+
+    const aleartMessage = noticeOrderMessage("Parn","Success","อเมริกาโน่");
+    expect(aleartMessage).toBe("อเมริกาโน่ ของคุณ Parn เสร็จแล้ว เชิญรับได้ที่เค้าเตอร์ค่ะ"); 
+ 
+  
+});
+
+test("notice order not Success", () => {
+
+    const aleartMessage = noticeOrderMessage("Noon","Not Success","ชาเย็น");
+
+    // เคสนี้ ถ้าไม่ใส่ StatusOrder ได้ เนื่องจาก จะไปตกเงื่อนไขสุดท้ายของ Fuction 
+    // const aleartMessage = noticeOrderMessage("Noon","","ชาเย็น");
+    expect(aleartMessage).toBe("กำลังทำชาเย็น ของคุณ Noon อยู่นะคะ กรุณารอสักครู่ค่ะ"); 
+
+  
 });
